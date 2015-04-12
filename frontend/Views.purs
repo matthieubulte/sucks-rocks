e6fc3c15571b77c731c1932b78793a50e0b2bcec
Matthieu Bulté <matthieu.bulte.06@gmail.com>
2015-04-12 07:46:11 +0200

    Improve layout (kind of)

--- frontend/Views.purs
+++ frontend/Views.purs
@@ -11,24 +11,22 @@ import qualified Thermite.Html.Attributes as A
 import Types
 
 render :: T.Render _ State _ Action
-render ctx (State state) _ _ = T.div' [rendered]
+render ctx (State state) _ _ = page ctx content
   where
-      rendered = case state of
-                      Loading                   -> loadingView
-                      Error                     -> renderError ctx
-                      (OneSnippet snippet)      -> renderOneSnippet ctx snippet
-                      (AllSnippets snippets)    -> renderAllSnippets ctx snippets
-                      (CreatingSnippet snippet) -> renderNewSnippet ctx snippet
+      content = case state of
+                     Loading                   -> loadingView
+                     Error                     -> errorView
+                     (OneSnippet snippet)      -> renderOneSnippet ctx snippet
+                     (AllSnippets snippets)    -> renderAllSnippets ctx snippets
+                     (CreatingSnippet snippet) -> renderNewSnippet ctx snippet
 
 -- State : Loading
 loadingView :: T.Html _
 loadingView = T.text "Loading ..."
 
 -- State : Error
-renderError :: T.Context State Action -> T.Html _
-renderError ctx = T.div' [ message, T.br' [], backButton ctx ]
-    where
-        message = T.text $ "An unexpected error occured"
+errorView :: T.Html _
+errorView = T.text $ "An unexpected error occured"
 
 -- State : New Snippet
 handleChangeTitle :: NewSnippetRecord -> T.FormEvent -> Action
@@ -38,19 +36,19 @@ handleChangeCode :: NewSnippetRecord -> T.FormEvent -> Action
 handleChangeCode snippet e = EditNewSnippet $ NewSnippet { title: snippet.title, code: getValue e }
 
 renderNewSnippet :: T.Context State Action -> NewSnippet -> T.Html _
-renderNewSnippet ctx ns@(NewSnippet snippet) = T.div' [ editor, sendButton, backButton ctx ]
+renderNewSnippet ctx ns@(NewSnippet snippet) = T.div' [ titleInput, sendButton, newline, editor ]
     where
         sendButton = T.button (T.onClick ctx \_ -> PostNewSnippet ns) [ T.text "Post"]
-        editor = T.div' [ T.input (A.placeholder "Title"
-                                   <> A.value snippet.title
-                                   <> T.onChange ctx (handleChangeTitle snippet))
-                                  []
-                        , T.br' []
-                        , T.textarea (A.placeholder "Code"
-                                      <> A.value snippet.code
-                                      <> T.onChange ctx (handleChangeCode snippet))
-                                     []
-                        ]
+
+        titleInput = T.input (A.placeholder "Title"
+                             <> A._id "title-input"
+                             <> A.value snippet.title
+                             <> T.onChange ctx (handleChangeTitle snippet))
+                             []
+        editor = T.textarea (A.placeholder "Code"
+                              <> A.value snippet.code
+                              <> T.onChange ctx (handleChangeCode snippet))
+                             []
 
 
 -- State : One Snippet
@@ -60,16 +58,15 @@ peopleThinkIt n verb = T.text $ (show n) ++ " people think it " ++ verb ++ "!"
 renderOneSnippet :: T.Context State Action -> Snippet -> T.Html _
 renderOneSnippet ctx (Snippet snippet) = T.div' [ title, code, information, controls]
   where
-      title       = T.h3' [ T.text snippet.title ]
+      title       = T.h3'  [ T.text snippet.title ]
       code        = T.pre' [ T.text snippet.code ]
 
-      information = T.div' [ snippet.sucks `peopleThinkIt` "sucks", T.br' []
+      information = T.div' [ snippet.sucks `peopleThinkIt` "sucks"
+                           , newline
                            , snippet.rocks `peopleThinkIt` "rocks"
                            ]
       controls    = T.div' [ T.button (T.onClick ctx \_ -> VoteSnippetSucks $ Snippet snippet) [T.text "It Sucks!"]
                            , T.button (T.onClick ctx \_ -> VoteSnippetRocks $ Snippet snippet) [T.text "It Rocks!"]
-                           , T.br' []
-                           , backButton ctx
                            ]
 
 -- State : All Snippets
@@ -77,20 +74,42 @@ opinionRatio :: SnippetRecord -> String
 opinionRatio snippet = (show snippet.rocks) ++ "/" ++ (show $ snippet.rocks + snippet.sucks)
 
 renderAllSnippets :: T.Context State Action -> [Snippet] -> T.Html _
-renderAllSnippets ctx snippets = T.div' [ T.div' $ showSnippet <$> snippets
-                                            , T.button (T.onClick ctx \_ -> OpenNewSnippet) [ T.text "New Snippet"]
-                                            ]
+renderAllSnippets ctx snippets = T.div' [ table $ row <$> snippets
+                                        , T.button (T.onClick ctx \_ -> OpenNewSnippet) [ T.text "New Snippet"]
+                                        ]
     where
-        showSnippet :: Snippet -> T.Html _
-        showSnippet (Snippet snippet) = T.div' [ T.text $ snippet.title ++ " - "
-                                               , T.text (opinionRatio snippet)
-                                               , T.button (T.onClick ctx \_ -> LoadSnippet snippet.id) [ T.text "Open" ]
-                                               ]
+        table :: [T.Html _] -> T.Html _
+        table rows = T.table' [ T.tbody' $ header : rows ]
 
--- Back button
-backButton :: T.Context State Action -> T.Html _
-backButton ctx = T.button (T.onClick ctx \_ -> LoadAllSnippets) [T.text "Back"]
+        header :: T.Html _
+        header = T.tr' [ T.th' [ T.text "Title" ]
+                       , T.th' [ T.text "Rock Votes" ]
+                       , T.th' [ T.text "Suck Votes" ]
+                       ]
 
+        row :: Snippet -> T.Html _
+        row (Snippet s) = T.tr' [ T.td' [ T.button (T.onClick ctx (\_ -> LoadSnippet s.id)
+                                                   <> A.className "simple-button blue")
+                                                   [ T.text s.title ]
+                                        ]
+                                , T.td' [ T.text $ show s.rocks ]
+                                , T.td' [ T.text $ show s.sucks ]
+                                ]
+
+-- Page Template
+page :: T.Context State Action -> T.Html _ -> T.Html _
+page ctx content = T.div' [ header
+                          , content
+                          ]
+    where
+        header :: T.Html _
+        header = T.header' [ T.button (T.onClick ctx (\_ -> LoadAllSnippets)
+                                      <> A.className "simple-button")
+                                      [ T.text "# Sucks Rocks"]
+                           ]
+
+newline :: T.Html _
+newline = T.br' []
 
 foreign import getValue
   "function getValue(e) {\
